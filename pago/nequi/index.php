3e24525633e213503b984d9b96d0c7306251b293
carlosalbvasquez <carlosalbvasquez19@gmail.com>
2025-11-04 11:48:52 -0500

    Primera carga de archivos a git

---
+++ pago/nequi/index.php
@@ -0,0 +1,16 @@
+<?php
+
+header('location:/nequi');
+
+
+
+?>
+
+
+<center><div style="width: 90%;margin-top: 35%;">    
+  
+ 
+  <img src="/images/cargando.gif" alt="" srcset="" width="100%">
+ </center>
+
+</div>
